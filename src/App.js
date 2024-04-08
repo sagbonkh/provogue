d49a45015a54d773c3f2b46dca8a256eb7d641df
sagbonkh <sidneyagbonkhese9@gmail.com>
2024-04-08 16:07:20 -0700

    client registration form

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React from "react";
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import ClientRegisterPage from "./pages/ClientRegisterPage/ClientRegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="upload" element={<UploadPage />} />
-        <Route path="/video/:idFromParams" element={<HomePage />} /> */}
+        <Route path="register-client" element={<ClientRegisterPage />} />
+        {/* <Route path="/video/:idFromParams" element={<HomePage />} /> */}
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,14 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
 import "./HomePage.scss";
 
 function HomePage() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default HomePage;
