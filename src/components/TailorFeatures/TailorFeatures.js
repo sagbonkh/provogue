0df9bc65a9f2a2eb13507050a33492b0bbc839d0
sagbonkh <sidneyagbonkhese9@gmail.com>
2024-04-11 05:44:39 -0700

    Style changes

--- src/components/TailorFeatures/TailorFeatures.js
+++ src/components/TailorFeatures/TailorFeatures.js
@@ -3,13 +3,28 @@ import placeholder1 from "../../assets/images/placeholder.png";
 import placeholder2 from "../../assets/images/placeholder.png";
 import placeholder3 from "../../assets/images/placeholder.png";
 import placeholder4 from "../../assets/images/placeholder.png";
-import "./TailorFeatures.scss"; // Import your SCSS file
+import "./TailorFeatures.scss";
 
 const TailorFeatures = () => {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const renderImages = () => {
-    if (window.innerWidth < 768) {
+    if (screenSize < 768) {
       return (
         <div className="container">
+          {/* Placeholder 1 */}
           <div className="tailor-features__container">
             <img
               src={placeholder1}
@@ -25,6 +40,7 @@ const TailorFeatures = () => {
               </p>
             </div>
           </div>
+          {/* Placeholder 2 */}
           <div className="tailor-features__container">
             <img
               src={placeholder2}
@@ -39,6 +55,7 @@ const TailorFeatures = () => {
               </p>
             </div>
           </div>
+          {/* Placeholder 3 */}
           <div className="tailor-features__container">
             <img
               src={placeholder3}
@@ -54,6 +71,7 @@ const TailorFeatures = () => {
               </p>
             </div>
           </div>
+          {/* Placeholder 4 */}
           <div className="tailor-features__container">
             <img
               src={placeholder4}
@@ -75,18 +93,45 @@ const TailorFeatures = () => {
       // Render a single image and text section for screens larger than 768px
       return (
         <div className="tailor-features__container">
-          <img
-            src={placeholder1}
-            alt="Placeholder 1"
-            className="tailor-features__img"
-          />
-          <div className="tailor-features__section">
-            <h3 className="tailor-features__subtitle">Client Management</h3>
-            <p className="tailor-features__text">
-              Effortlessly oversee client bookings, appointments, and projects
-              from a centralized platform, ensuring a seamless experience for
-              both you and your clients.
-            </p>
+          <div className="img_div">
+            <img
+              src={placeholder1}
+              alt="Placeholder 1"
+              className="tailor-features__img"
+            />
+          </div>
+          <div className="tailor-features__sections">
+            <div className="tailor-features__section">
+              <h3 className="tailor-features__subtitle">Client Management</h3>
+              <p className="tailor-features__text">
+                Effortlessly oversee client bookings, appointments, and projects
+                from a centralized platform, ensuring a seamless experience for
+                both you and your clients.
+              </p>
+            </div>
+            <div className="tailor-features__section">
+              <h3 className="tailor-features__subtitle">Invoices</h3>
+              <p className="tailor-features__text">
+                Craft polished, branded invoices effortlessly and get paid
+                faster with our online invoicing solution.
+              </p>
+            </div>
+            <div className="tailor-features__section">
+              <h3 className="tailor-features__subtitle">Contracts</h3>
+              <p className="tailor-features__text">
+                Effortlessly send professional contracts for electronic
+                signature, empowering clients to sign securely from any device,
+                at any time.
+              </p>
+            </div>
+            <div className="tailor-features__section">
+              <h3 className="tailor-features__subtitle">Quotes</h3>
+              <p className="tailor-features__text">
+                Effortlessly send professional contracts for electronic
+                signature, empowering clients to sign securely from any device,
+                at any time.
+              </p>
+            </div>
           </div>
         </div>
       );
