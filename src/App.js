a0c20be8ff3b59d12fa45ed357e4641c5e8dedf2
sagbonkh <sidneyagbonkhese9@gmail.com>
2024-04-13 20:41:35 -0700

    Made some changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ClientBooking from "./pages/ClientBooking/ClientBooking";
 import TailorDashboard from "./pages/TailorDashboard/TailorDashboard";
 import TailorRedirect from "./pages/TailorRedirect/TailorRedirect";
 import TailorsClients from "./pages/TailorsClients/TailorsClients";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/tailor/:id" element={<TailorDashboard />} />
         <Route path="/redirect-tailor" element={<TailorRedirect />} />
         <Route path="/tailor/:id/clients" element={<TailorsClients />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoginPage";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Log In</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
